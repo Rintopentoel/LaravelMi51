73af5ac9ac06de1cd0faee0f842fee949b619234
Grisha Zavodskoy <rintowaluyo_2225110010@mhs.mdp.ac.id>
2024-09-17 18:42:02 +0700

    migration

---
+++ kape/app/Http/Controllers/KomputerController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\komputer;
+use Illuminate\Http\Request;
+
+class KomputerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(komputer $komputer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(komputer $komputer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, komputer $komputer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(komputer $komputer)
+    {
+        //
+    }
+}
---
+++ kape/database/migrations/2024_09_17_103946_create_jadwal_tims_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jadwal_tims', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('jadwal_id');
+            $table->foreign('jadwal_id')->references('id')->on('jadwals');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jadwal_tims');
+    }
+};
